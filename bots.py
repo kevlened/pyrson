4fabf0d49eb1591d3a88c807f10b8cf707e4e1c9
Len Boyette <boyettel@gmail.com>
2012-09-09 13:25:46 -0400

    Added RiveScript support by default

--- bots.py
+++ bots.py
@@ -10,6 +10,9 @@
 #-------------------------------------------------------------------------------
 
 import aiml
+from rivescript import RiveScript
+
+failed_response = "I don't know what to say"
 
 class PyAIML():
     def respond_to(self, text):
@@ -21,3 +24,20 @@ class PyAIML():
 
     def __init__(self):
         self.k = aiml.Kernel()
+
+class RiveScriptBot():
+    def respond_to(self, text, user="localuser"):
+        response = self.k.reply(user,text)
+        rivescript_failed_responses = set(["ERR: No Reply Found" , "ERR: No Reply Matched"])
+        if response in rivescript_failed_responses:
+            response = failed_response
+        return response
+
+    def learn(self, filefoldername):
+        #need to see if it is a file or folder - for now we'll assume it's the
+        #brain folder
+        self.k.load_directory(filefoldername)
+        self.k.sort_replies()
+
+    def __init__(self):
+        self.k = RiveScript()
--- pyrson_main.py
+++ pyrson_main.py
@@ -12,37 +12,35 @@
 from events import EventHook
 from stt import dragonfly_stt
 from tts import TTSX
-from bots import PyAIML
-from action import dragonfly_action
+from bots import RiveScriptBot
+#from action import dragonfly_action
 
 stt_library = dragonfly_stt()
 tts_library = TTSX()
-bot_library = PyAIML()
-action_library = dragonfly_action()
+bot_library = RiveScriptBot()
+#action_library = dragonfly_action()
 #vision_library = PyOpenCV()
 
 def main():
     stt_library.onNewText += new_text
-    bot_library.learn("AIML/callmom_weather.aiml")
+    bot_library.learn("./RiveScript")
 
-    action_library.load("actions.txt")
+    #action_library.load("actions.txt")
 
-    text = "How is the weather in Raleigh"
+    text = "Hello Bot"
     new_text(text)
+
     stt_library.start()
 
 def new_text(text):
     text = str(text)
-    print "you: " + text
+    print text
 
-##    acted_on, response = action_library.act_on(text)
-##
-##    if acted_on == False:
-##        response = bot_library.respond_to(text)
+    #acted_on, response = action_library.act_on(text)
 
     response = bot_library.respond_to(text)
-    print "bot: " + response
 
+    print response
     tts_library.say(response)
 
 if __name__ == '__main__':

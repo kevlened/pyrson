3633612bbee42c11490924869011b8b953f9f992
Len Boyette <boyettel@gmail.com>
2012-09-27 00:27:14 -0400

    Changed bot folder structure

---
+++ bots/PyAIMLBot.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Owner
+#
+# Created:     23/08/2012
+# Copyright:   (c) Owner 2012
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import pyaiml
+
+failed_response = "I don't know what to say"
+
+class PyAIMLBot():
+    def respond_to(self, text):
+        response = self.k.respond(text)
+        return response
+
+    def learn(self, filefoldername):
+        self.k.learn(filefoldername)
+
+    def __init__(self):
+        self.k = aiml.Kernel()
